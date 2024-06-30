d8929bd8b911cd4a0d497037e725f31da7cebc94
Label Broadcast <info.labelbroadcast@gmail.com>
2024-06-30 17:34:31 +0800

    commit

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,9 +1,43 @@
+import { Play, Plus, ThumbsDown, ThumbsUp } from "lucide-react";
 import "./listItem.scss";
+import { useState } from "react";
 
-export default function ListItem() {
+export default function ListItem({ index }) {
+    const [isHovered, setIsHovered] = useState(false);
+    const trailer = "https://www.labelbroadcast.com/file/star_wars.mp4";
+
   return (
-    <div className="listItem">
-      item
+    <div 
+        className="listItem" 
+        style={{left:isHovered && index * 215 - 40 + index * 7}}
+        onMouseEnter={() => setIsHovered(true)} 
+        onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src="/img/favourites/f5.webp" alt="thumbnail" />
+      {isHovered && (
+       <>
+        <video src={trailer} autoPlay={true} loop />
+        <div className="itemInfo">
+            <div className="icons">
+                <div>
+                    <Play className="icon" size={18} />
+                    <Plus className="icon"size={18}  />
+                    <ThumbsUp className="icon" size={18} />
+                    <ThumbsDown className="icon" size={18} />
+                </div>
+                <div className="genre">Action</div>
+            </div>
+            <div className="itemInfoTop">
+                <span>1hr 14min</span>
+                <span className="limit">PG</span>
+                <span>2008</span>
+            </div>
+            <div className="desc">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum incidunt quam rerum, quos vero quae perspiciatis earum vitae corrupti eligendi deserunt sit assumenda. Temporibus error, officiis eos obcaecati quod sequi?
+            </div>
+        </div>
+       </>
+      )}
     </div>
   )
 }
